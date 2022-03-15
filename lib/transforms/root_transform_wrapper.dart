1f6686e4d1631bd9fe3f3fbb02ecd0e66b3fed30
xavi <xavings@gmail.com>
2022-03-15 08:54:25 +0100

    With previous commit

--- lib/transforms/root_transform_wrapper.dart
+++ lib/transforms/root_transform_wrapper.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ipfoam_client/navigation.dart';
-import 'package:ipfoam_client/transforms/interplanetary_text/dynamic_transclusion_run.dart';
 import 'package:ipfoam_client/transforms/interplanetary_text/interplanetary_text.dart';
-import 'package:ipfoam_client/transforms/page_navigator.dart';
 import 'package:provider/provider.dart';
 
 class RootTransformWrapper extends StatefulWidget {
@@ -28,10 +26,5 @@ class RootTransformWrapperState extends State<RootTransformWrapper> {
     var iptRun = IPTFactory.makeIptRunFromExpr(expr, IptRoot.defaultOnTap);
 
     return IPTFactory.getRootTransform(expr, IptRoot.defaultOnTap);
-    var dynamicRun = iptRun as DynamicTransclusionRun;
-    return PageNavigator(
-      arguments: dynamicRun.arguments,
-      key: ValueKey("pageNavigator"),
-    );
   }
 }
