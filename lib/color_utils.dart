ea9026abac1e73c64710bf605aa549cb0affec7f
xavi <xavings@gmail.com>
2022-06-10 10:54:21 +0200

    cosmetic tweaks

--- lib/color_utils.dart
+++ lib/color_utils.dart
@@ -24,8 +24,8 @@ Color getBackgroundColor(String source) {
   double saturation = 1; //0 black and white, 1 normal
   double lightness = 0.5; //0 black, 0.5 normal,  1 white
   Color tintColor = Colors.yellow;
-  double tintAmount = 0.5; //0 no tint applied, 1 totally tinted
-  double opacity = 0.4; //0 transparent, 1 normal
+  double tintAmount = 0.35; //0 no tint applied, 1 totally tinted
+  double opacity = 0.2; //0 transparent, 1 normal
   double hue = strToHue(source);
   HSLColor hsl = HSLColor.fromAHSL(opacity, hue, saturation, lightness);
   Color rgb = hsl.toColor();
