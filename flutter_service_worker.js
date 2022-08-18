33a93df5f6c32f98bd949796b6534ae72e2a150d
xavi <xavings@gmail.com>
2022-08-18 21:15:44 +0200

    stuff

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,9 +5,9 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "03e700e5a83770ec6a50de5460de639c",
 "favicon.ico": "3f6fd881000049e345f9a5ecfecdd4a1",
-"index.html": "e7d11024c7e265ff381e3c3dbcc22b00",
-"/": "e7d11024c7e265ff381e3c3dbcc22b00",
-"main.dart.js": "a9a6b0afb083b61a847c470b35af4f54",
+"index.html": "b6cc92c8994a2b6a36502d4d1095138f",
+"/": "b6cc92c8994a2b6a36502d4d1095138f",
+"main.dart.js": "1bdefc8ea7a8a6cb43922302b797ea15",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
